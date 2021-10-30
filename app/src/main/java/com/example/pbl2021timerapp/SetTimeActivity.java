79fcfb36ffed21613f5146ea85925ae0ebccd9a7
Toya-Onodera <gtrzexel@gmail.com>
2021-10-31 02:11:48 +0900

    Add: 時刻を選択したあとに TextView へ描画する機能を追加

--- app/src/main/java/com/example/pbl2021timerapp/SetTimeActivity.java
+++ app/src/main/java/com/example/pbl2021timerapp/SetTimeActivity.java
@@ -3,17 +3,24 @@ package com.example.pbl2021timerapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SetTimeActivity extends AppCompatActivity {
+    private TextView setTimeTextView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_time);
 
-        findViewById(R.id.openDialogFragmentButton).setOnClickListener(view -> {
+        setTimeTextView = findViewById(R.id.setTimeView);
+        setTimeTextView.setOnClickListener(view -> {
             DialogFragment newFragment = new TimePickerFragment();
             newFragment.show(getSupportFragmentManager(), "timePicker");
         });
+    }
+
+    public void onReturnChooseTime(int hourOfDay, int minute) {
+        setTimeTextView.setText(String.format("%02d:%02d", hourOfDay, minute));
     }
 }
--- app/src/main/java/com/example/pbl2021timerapp/TimePickerFragment.kt
+++ app/src/main/java/com/example/pbl2021timerapp/TimePickerFragment.kt
@@ -1,24 +1,24 @@
-package com.example.pbl2021timerapp;
+package com.example.pbl2021timerapp
+
 import android.app.Dialog
 import android.app.TimePickerDialog
 import android.os.Bundle
-import android.text.format.DateFormat
 import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
 class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // Use the current time as the default values for the picker
         val c = Calendar.getInstance()
         val hour = c.get(Calendar.HOUR_OF_DAY)
         val minute = c.get(Calendar.MINUTE)
-
-        // Create a new instance of TimePickerDialog and return it
-        return TimePickerDialog(activity, this, hour, minute, DateFormat.is24HourFormat(activity))
+        return TimePickerDialog(activity, this, hour, minute, false)
     }
 
-    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-        // Do something with the time chosen by the user
+    // Fragment から Activity 値を渡すには呼び出し元のメソッドを直接呼ぶ必要がある
+    // https://www.ipentec.com/document/android-custom-dialog-using-dialogfragment-return-value
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        val callingActivity = activity as SetTimeActivity
+        callingActivity.onReturnChooseTime(hourOfDay, minute)
     }
 }
