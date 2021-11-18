ec2ee2eb580221b2c55741805eea79245d9b3ca9
Daichi-Ikarashi <g031r007@s.iwate-pu.ac.jp>
2021-11-18 17:50:06 +0900

    ボタンを押したら音楽を再生して、別のボタンを押したら止める機能を実装したかったが、ボタンを押したらダイアログを表示させるところまで終わった。

---
+++ app/src/main/java/com/example/pbl2021timerapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.testapp01;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder
+                        .setTitle(R.string.dialog_title)
+                        .setMessage(R.string.dialog_message)
+                        .show();
+            }
+        });
+    }
+}
