7a4ac638b331c7db145fbf9919631d6fcc0b2280
Toya-Onodera <gtrzexel@gmail.com>
2021-11-22 14:58:33 +0900

    Add: 3秒後に Activity を表示する動作を実装

--- app/src/main/java/com/example/pbl2021timerapp/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/example/pbl2021timerapp/AlarmBroadcastReceiver.java
@@ -4,10 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.example.pbl2021timerapp.view.set_time.SetTimeActivity;
+
 public class AlarmBroadcastReceiver extends BroadcastReceiver
 {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Received ", Toast.LENGTH_LONG).show();
+
+        Intent i = new Intent();
+        i.setClassName("com.example.pbl2021timerapp", "com.example.pbl2021timerapp.GoTimerActivity");
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
 }
