5b4b79de442d90d30f5febea498ffcb69c1c5619
Toya-Onodera <gtrzexel@gmail.com>
2021-11-29 15:18:12 +0900

    add: 音声再生機能実装

--- app/src/main/java/com/example/pbl2021timerapp/GoTimerActivity.java
+++ app/src/main/java/com/example/pbl2021timerapp/GoTimerActivity.java
@@ -2,13 +2,45 @@ package com.example.pbl2021timerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class GoTimerActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_timer);
+
+        // MediaPlayer のインスタンス生成
+        mediaPlayer = new MediaPlayer();
+
+        try {
+            AssetFileDescriptor afdescripter = getAssets().openFd("01.mp3");
+
+            // MediaPlayerに読み込んだ音楽ファイルを指定
+            mediaPlayer.setDataSource(afdescripter.getFileDescriptor(),
+                    afdescripter.getStartOffset(),
+                    afdescripter.getLength());
+
+            // 音量調整を端末のボタンに任せる
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+            // 音声再生準備と再生
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 }
+
